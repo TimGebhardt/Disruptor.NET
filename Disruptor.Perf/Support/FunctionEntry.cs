8c2bb07e4047aa62910fd5a79cd1920cff0e8384
Tim Gebhardt <tim@gebhardtcomputing.com>
2011-07-02 06:44:44 -0500

    Moving FunctionEntry and FunctionHandler to their own code files and cleaning up the API for them.

---
+++ Disruptor.Perf/Support/FunctionEntry.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: timbo
+ * Date: 7/2/2011
+ * Time: 6:35 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Disruptor.Perf.Support
+{
+	public class FunctionEntry : AbstractEntry
+	{
+		public long OperandOne { get; set; }
+		public long OperandTwo { get; set; }
+		public long StepOneResult { get; set; }
+		public long StepTwoResult { get; set; }
+	}
+	
+	internal class FunctionEntryFactory : IEntryFactory<FunctionEntry>
+	{
+		public FunctionEntry Create()
+		{
+			return new FunctionEntry();
+		}
+	}
+}
---
+++ Disruptor.Perf/Support/FunctionHandler.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: timbo
+ * Date: 7/2/2011
+ * Time: 6:41 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Disruptor.Perf.Support
+{
+	public class FunctionHandler : IBatchHandler<FunctionEntry>
+	{
+		private readonly FunctionStep functionStep;
+		private long stepThreeCounter;
+
+		public FunctionHandler(FunctionStep functionStep)
+		{
+			this.functionStep = functionStep;
+		}
+
+		public long getStepThreeCounter()
+		{
+			return stepThreeCounter;
+		}
+
+		public void reset()
+		{
+			stepThreeCounter = 0L;
+		}
+
+
+		public void OnAvailable(FunctionEntry entry)
+		{
+			switch (functionStep)
+			{
+				case FunctionStep.ONE:
+					entry.StepOneResult = entry.OperandOne + entry.OperandTwo;
+					break;
+
+				case FunctionStep.TWO:
+					entry.StepTwoResult = entry.StepOneResult + 3L;
+					break;
+
+				case FunctionStep.THREE:
+					if((entry.StepTwoResult & 4L) & 4L)
+						stepThreeCounter++;
+					break;
+			}
+		}
+
+		public void OnEndOfBatch()
+		{
+		}
+
+
+		public void OnCompletion()
+		{
+		}
+	}
+}
